cb917187191e077ae7f326f786c45bd52d32c775
Olivia Gwynn <oliviagwynn@duke.edu>
2018-10-22 19:12:05 -0400

    Import Data in Lists

--- hrm.py
+++ hrm.py
@@ -4,19 +4,22 @@ def csv(f):
     with open(f) as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
 
-        dict = {}
-        dict["time"] = []
-        dict["voltage"] = []
+        time = []
+        voltage = []
         #Read file row by row
         rowNr = 0
         for row in reader:
             if rowNr >= 0:
-                dict["time"].append(float(row[0]))
-                dict["voltage"].append(float(row[1]))
+                time.append(float(row[0]))
+                voltage.append(float(row[1]))
             rowNr = rowNr + 1
-        return dict
+        return time, voltage
 
 
 if __name__ == "__main__":
+    #Need to call all data sets
     data = csv('test_data1.csv')
-    print(data)
+    time = data[0]
+    voltage = data[1]
+    print(time)
+    print(voltage)
