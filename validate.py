f757ed8aa53ec3dcf3f8a3b2d9ee6cf538395cd7
Olivia Gwynn <oliviagwynn@duke.edu>
2018-10-23 16:22:11 -0400

    First commit for validate

---
+++ validate.py
@@ -0,0 +1,11 @@
+def validate():
+    """Check lengths of time and voltage are the same"""
+    """Check time has no negative data"""
+    ###Convert to numpy array and find where values are negative, and produce array of True, False etc
+    ###Use np.where(file) to find index of false values
+    #    for value in time_array:
+    #        if value < 0:
+    #            return false
+    #    return true
+    """Check voltage does not exceed 300mv"""
+pass
