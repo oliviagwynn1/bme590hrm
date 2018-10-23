f17d514b4cf7adf1d8bff00d17fd2e2aef65a178
Olivia Gwynn <oliviagwynn@duke.edu>
2018-10-23 16:16:01 -0400

    First commit of mean_hr_bpm

---
+++ mean_hr_bpm.py
@@ -0,0 +1,4 @@
+def mean_hr_bpm():
+    """Estimated average heart rate over a user-specified number of minutes (can choose a default interval)"""
+    mean_hr_bmp_val = 'Fill this in.'
+    return mean_hr_bmp_val
