f4e1f4a0d29894a24cfd249b51174f8a5bff5395
Olivia Gwynn <oliviagwynn@duke.edu>
2018-10-24 10:28:12 -0400

    Duration count working

--- duration.py
+++ duration.py
@@ -1,4 +1,13 @@
-def duration():
+import numpy as np
+
+time = [0,1,2,3,4,5,6,10]
+time_array = np.array(time)
+
+
+def duration(time_array):
     """Time duration of the ECG strip"""
-    duration_val = "Fill this in."
+    length = len(time_array)
+#    return length
+    duration_val = time_array[7]
     return duration_val
+print(duration(time_array))
