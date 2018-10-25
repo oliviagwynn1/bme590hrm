d93f2bde3e3127886cc69b98f7bf2e34baf5bbc9
Olivia Gwynn <oliviagwynn@duke.edu>
2018-10-25 11:52:32 -0400

    Testing data with missing values

--- reader.py
+++ reader.py
@@ -1,16 +1,32 @@
-def csv(f):
+import matplotlib.pyplot as plt
+#a= [[1,2], [1,], [c,9], [j,hello]]
+
+def csv2(f):
     import csv
+    import numpy as np
 
     with open(f) as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
 
+
         time = []
         voltage = []
+
         #Read file row by row
-        rowNr = 0
         for row in reader:
-            if rowNr >= 0:
-                time.append(float(row[0]))
-                voltage.append(float(row[1]))
-            rowNr = rowNr + 1
+            time.append(float(row[0]))
+            voltage.append(float(row[1]))
+            #except ValueError:
+            #    print("Input data is missing a numeric value")
+            
+            #del time[row]
+            #del voltage[row]
         return np.array(time), np.array(voltage)
+
+if __name__ == "__main__":
+    data = csv2('test_data1.csv')
+    time = data[0]
+    voltage = data[1]
+
+    plt.plot(time, voltage)
+    plt.show()
