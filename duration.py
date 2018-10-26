29a88069ac329c892151969c05053850f5ec14cd
Olivia Gwynn <oliviagwynn@duke.edu>
2018-10-25 23:47:29 -0400

    Updated duartion unit test to have more efficient code.

--- duration.py
+++ duration.py
@@ -1,12 +1,5 @@
-import numpy as np
-
-time_array = [0, 9, 50]
-#time_array = np.array(time)
-#print(time_array)
-
 def duration(time_array):
     """Time duration of the ECG strip"""
     length = len(time_array) - 1
     duration_val = time_array[length]
     return duration_val
-print(duration(time_array))
--- test_duration.py
+++ test_duration.py
@@ -1,15 +1,5 @@
-from duration import duration
-import pytest
-import numpy as np
-
-
-
-@pytest.mark.parametrize("time_array, expected",[
-    ("[0, 4, 20]", 20),
-    ("[9, 10, 100]", 100),
-    ("[10]", 10)
-])
-
-def test_duration(time_array, expected):
-    response = duration(time_array)
-    assert response == expected
+def test_duration():
+    from duration import duration
+    import numpy as np
+    response = duration(time_array=np.array([0, 9, 30, 40]))
+    assert response == 40
