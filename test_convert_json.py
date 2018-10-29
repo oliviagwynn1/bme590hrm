f0c236c59e878579a434987ea0b5ca34c46095b4
Olivia Gwynn <oliviagwynn@duke.edu>
2018-10-29 00:11:13 -0400

    Code working.

---
+++ test_convert_json.py
@@ -0,0 +1,8 @@
+# def test_convert_json():
+#     from convert_json import convert_json
+#     response = convert_json(myDictionary={"List": [1,2,3],
+#                                           "Tuple": (1,2,3),
+#                                           "String": "Hello"
+#                                           })
+#
+#     assert response == {"List": [1, 2, 3], "Tuple": [1, 2, 3], "String": "Hello"}
